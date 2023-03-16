7061016a92fbbcccaeaa8e6502127b0afc122437
Etash Ashwin <128017545+Etash1601@users.noreply.github.com>
2023-03-16 10:28:04 +0530

    Create Math_functions.java

---
+++ Math_functions.java
@@ -0,0 +1,15 @@
+import java.lang.Math;
+public class Main {
+    public static void main(String[] args)
+    {
+        double a = 30;
+        double b = 2;
+        double d = 145.256;
+        double c = 1.0;
+        float x = 4567.9874f;
+        System.out.println(Math.pow(a,b));
+        System.out.println(Math.log(d));
+        System.out.println(Math.round(x));
+        System.out.println(Math.asin(c));
+    }
+}
