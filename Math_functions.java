fbed2be250a848e4262656b4109d0d3c055db460
Etash Ashwin <128017545+Etash1601@users.noreply.github.com>
2023-03-16 10:30:25 +0530

    Update Math_functions.java

--- Math_functions.java
+++ Math_functions.java
@@ -7,9 +7,9 @@ public class Main {
         double d = 145.256;
         double c = 1.0;
         float x = 4567.9874f;
-        System.out.println(Math.pow(a,b));
-        System.out.println(Math.log(d));
-        System.out.println(Math.round(x));
-        System.out.println(Math.asin(c));
+        System.out.println(Math.pow(a,b));  // 900.0
+        System.out.println(Math.log(d));  // 4.978497702968366
+        System.out.println(Math.round(x));  // 4568
+        System.out.println(Math.asin(c));  // 1.5707963267948966
     }
 }
